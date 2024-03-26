653ba151eb32afb485f83ccc2b37918c5cae5762
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-03-26 15:52:46 -0300

    modified:   tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,11 +1,14 @@
 
+grid = [[1,'|',2,'|',3],
+        ['-','+','-','+','-'],
+        [4,'|',5,'|',6],
+        ['-','+','-','+','-'],
+        [7,'|',8,'|',9]]
 
 def generates_grid():
-    print('1|2|3')
-    print('-+-+-')
-    print('4|5|6')
-    print('-+-+-')
-    print('7|8|9')
+    for line in grid:
+            print(f'{line[0]}|{line[2]}|{line[4]}')
+    
 
 def main():
     generates_grid()
