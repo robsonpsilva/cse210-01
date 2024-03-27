31732d6b592d6b0c5088127c41915aa6b6bbe39a
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-03-27 12:02:33 -0300

    modified:   test_tic_tac_toe.py 	modified:   tic_tac_toe.py

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -1,7 +1,50 @@
-from tic_tac_toe import generates_grid, select_player_input
+from tic_tac_toe import print_grid, generates_and_fill_grid
 import pytest
+import collections
 
+def test_print_grid():
+    grid = [['1','|','2','|','3'],
+        ['-','+','-','+','-'],
+        ['4','|','5','|','6'],
+        ['-','+','-','+','-'],
+        ['7','|','8','|','9']]
+    string1 = print_grid(grid)
+    string2 = '1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n'
+    assert string1 == string2
 
+def test_generates_and_fill_grid():
+    #Initialize grid with its default configuration
+    grid = [['1','|','2','|','3'],
+        ['-','+','-','+','-'],
+        ['4','|','5','|','6'],
+        ['-','+','-','+','-'],
+        ['7','|','8','|','9']]
+    
+    #Changes the grid first test
+    grid = generates_and_fill_grid(grid, '9', 'o')
+    #Formats a new grid to comparison
+    grid_aux = [['1','|','2','|','3'],
+        ['-','+','-','+','-'],
+        ['4','|','5','|','6'],
+        ['-','+','-','+','-'],
+        ['7','|','8','|','o']]
+    row = 0
+    for line in grid:
+        assert line == grid_aux[row]
+        row += 1
+    
+    #Changes the grid second test
+    grid = generates_and_fill_grid(grid, '4', 'x')
+     #Formats a new grid to comparison
+    grid_aux = [['1','|','2','|','3'],
+        ['-','+','-','+','-'],
+        ['x','|','5','|','6'],
+        ['-','+','-','+','-'],
+        ['7','|','8','|','o']]
+    row = 0
+    for line in grid:
+        assert line == grid_aux[row]
+        row += 1
 
 
 # Call the main function that is part of pytest so that the
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,29 +1,72 @@
+
 user_input_flag = True
-
-grid = [[1,'|',2,'|',3],
+grid = [['1','|','2','|','3'],
         ['-','+','-','+','-'],
-        [4,'|',5,'|',6],
+        ['4','|','5','|','6'],
         ['-','+','-','+','-'],
-        [7,'|',8,'|',9]]
+        ['7','|','8','|','9']]
 
-def generates_grid():
+def generates_and_fill_grid(grid, option, mark =''):
+    """
+        This function generate and prints the tic-tac-toe grid
+        option receives the cell chosed by the use and mark his leter
+        'x' or 'o'
+    """
+    if option in ['1','2','3']:
+        grid[0][2 * (int(option)-1)] = mark
+    elif option in ['4','5','6']:
+        grid[2][2 * (int(option)-4)] = mark
+    elif option in ['7','8','9']:
+        grid[4][2 * (int(option)-7)] = mark
+
+    return grid
+
+def print_grid(grid):
+    #prints the grid in console
+    grid_string = ''
     for line in grid:
-            print(f'{line[0]}|{line[2]}|{line[4]}')
+        for cell in line:
+            grid_string += f'{cell}'
+        grid_string += '\n'
+    print(grid_string)
+    return grid_string
 
-def select_player_input():
-    if user_input_flag == False:
+
+def select_player_input(user_input_flag):
+    """
+    There are two main massages possible in tic-tac-toe.
+    This function serves to select between them. 
+    """
+    if user_input_flag == True:
         message = 'x\'s turn to choose a square (1-9):'
     else:
         message = 'o\'s turn to choose a square (1-9):'    
-    user_input_flag = not user_input_flag
     return message
     
 
-if __name__ == "__main__":
-    main()  
-    
-
 def main():
-    generates_grid()
+    flag = True
+    while True:
+         
+        answer = select_player_input(flag)
 
-main()
+        #Check if the data entry is integer number
+        if answer.isinstance(int) == True:
+            #if number checks if it is in range between 1 and 9
+            if answer in ['1','2','3','4','5','6','7','8','9']:
+                ...
+            else:
+                #Means data entry is out of the range
+                print('Error: Data entry must be a integer between 1 and 9.')
+                answer = input('Type q if you want to quit or everyone else to return to the application.')
+                if answer.lower() == 'q':
+                    break
+        else: #Mean data entry is not a integer number
+            print('Error: Data entry is not a integer number.')
+            answer = input('Type q if you want to quit or everyone else to return to the application.')
+            if answer.lower() == 'q':
+                break
+    print('Application end')          
+
+if __name__ == "__main__":
+    main() 
